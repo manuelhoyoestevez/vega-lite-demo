48ee5431a6c740c314e6e531c54dca311c74a135
Manuel Hoyo Estévez <manuel.hoyo@inetum.world>
2021-08-18 16:39:16 +0200

    Extracted spec to external file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import * as embed from 'vega-embed';
+import spec from '../assets/vega-example';
 
 @Component({
     selector: 'app-root',
@@ -9,23 +10,6 @@ import * as embed from 'vega-embed';
 export class AppComponent implements OnInit {
 
     ngOnInit() {
-        const spec = {
-            "$schema": "https://vega.github.io/schema/vega-lite/v2.json",
-            "description": "Simple bar chart",
-            "data": {
-                "values": [
-                    {"x": "A", "y": 28}, {"x": "y", "B": 55}, {"x": "C", "y": 43},
-                    {"x": "D", "y": 91}, {"x": "E", "y": 81}, {"x": "F", "y": 53},
-                    {"x": "G", "y": 19}, {"x": "H", "y": 87}, {"x": "I", "y": 52}
-                ]
-            },
-            "mark": "bar",
-            "encoding": {
-                "x": {"field": "x", "type": "ordinal"},
-                "y": {"field": "y", "type": "quantitative"}
-            }
-        };
-
         embed("#vis", spec, { actions: false });
     }
 
